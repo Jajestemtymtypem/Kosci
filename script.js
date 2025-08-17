6a0d4db08ce3a2f8e142475bb96c93ab222bf78b
Jajestemtymtypem <jajestemtymtypem@gmail.com>
2025-08-18 00:41:16 +0200

    Kości
    Widzę że jest lepiej z pisaniem funkcji. Mam na to jakiś plan robię to co mi podpowiada głowa.
    Minusy:
    Kod ma kilka krytycznych błędów łatanie na kolanie. Przejście po linijkach wykonuje się kilku krotnie odkryłem do dodając szanse do bojetnie jakiej funkcji. pętla for która zlicza ze zmiennej punkty od 0 do 5 i podaje sume oczek n akostkach. wykonuje sięto po 3 razy.  dziś próbowąłem naprawiać kod ale wydaje mi się ze zacznę od nowa zamykam kości na wersji 0.

--- script.js
+++ script.js
@@ -3,28 +3,33 @@ let figura=[ 0, 0, 0, 0, 0];
 const obrazKostki = [null, "icons/1.png", "icons/2.png", "icons/3.png", "icons/4.png", "icons/5.png", "icons/6.png"]
 let czyMoznaRzucac = true;
 let wynik = 0;
-let punkty = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let punkty = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let tura = 0;
 let kostki = [0, 0, 0, 0, 0, 0];
 const stanKostki = true;
+let szansa = 0;
 function rzutKostkaLosowo(){ 
     return Math.floor(Math.random() * 6) + 1;
 }
 
 function rzutKoscmi(){
     for(let i = 0; i< 5; i++){
+       
 if(dice[i] !== "hold"){
     wynikKostki = rzutKostkaLosowo();
     dice[i] = wynikKostki
     figura[i] = wynikKostki
     const img = document.getElementById([i]);
     img.src = obrazKostki[wynikKostki];
+    zliczanieKosci();
+    znajdzFigury();
+    przypisywanieWynikow()
     kostki = [0, 0, 0, 0, 0, 0];
 }
     }
 tura++;
-zliczanieKosci();
-znajdzFigury();
+
+
 if(tura > 3){
         tura = 0;
         dice=[ 0, 0, 0, 0, 0];
@@ -40,6 +45,7 @@ console.log(tura);
 console.log(figura);
 console.log(`kostki ${kostki}`);
 console.log(`punkty ${punkty}`);
+
 }
 
 function hold0(el){
@@ -47,7 +53,7 @@ function hold0(el){
         const stanKostki = el.classList.toggle(`selected`);
         if(stanKostki && tura !==0) {
             dice[0] = "hold";
-            console.log("klik");
+            console.log(" hold0");
             console.log(dice);
         }else{
             dice[0] = figura[0];
@@ -59,7 +65,7 @@ function hold1(el){
         const stanKostki = el.classList.toggle(`selected`);
         if(stanKostki && tura !==0) {
             dice[1] = "hold";
-            console.log("klik");
+            console.log("klik hold1");
             console.log(dice);
         }else{
             dice[1] = figura[1];
@@ -71,7 +77,7 @@ function hold2(el){
         const stanKostki = el.classList.toggle(`selected`);
         if(stanKostki && tura !==0) {
             dice[2] = "hold";
-            console.log("klik");
+            console.log("klik hold2");
             console.log(dice);
         }else{
             dice[2] = figura[2];
@@ -83,7 +89,7 @@ function hold3(el){
         const stanKostki = el.classList.toggle(`selected`);
         if(stanKostki && tura !==0) {
             dice[3] = "hold";
-            console.log("klik");
+            console.log("klik hold3");
             console.log(dice);
         }else{
             dice[3] = figura[3];
@@ -95,7 +101,7 @@ function hold4(el){
         const stanKostki = el.classList.toggle(`selected`);
         if(stanKostki && tura !==0) {
             dice[4] = "hold";
-            console.log("klik");
+            console.log("klik hold4");
             console.log(dice);
         }else{
             dice[4] = figura[4];
@@ -123,6 +129,7 @@ function zliczanieKosci(){
         kostki[5] += 1; 
     }      
    }
+   
 }
 function znajdzFigury(){
     if(kostki[0] !== 0){
@@ -143,6 +150,8 @@ function znajdzFigury(){
     if(kostki[5] !== 0){
       punkty[5] = kostki[5] * 6;  
     }
+    
+    
     for( let i = 0; i < 6; i++){
         if(kostki[i] >= 3){
             punkty[6] = ((i+1) * 10);
@@ -150,10 +159,22 @@ function znajdzFigury(){
             for( let j = 0; j < 6; j++){
                 if(kostki[j] === 2&& kostki[j] != kostki[i]){
                     punkty[8] = ((i + j) * 8);
-                    console.log(`Full ${i} i ${j}`);
+                    console.log(`Full ${i+1} i ${j+1}`);
                 }
             }
         }
+    if(kostki[0] === 1 && kostki[1] === 1 && kostki[2] === 1 && kostki[3] === 1){
+        punkty[9] = 75;
+        console.log("Mały strit");
+    }
+    if(kostki[4] === 1 && kostki[1] === 1 && kostki[2] === 1 && kostki[3] === 1){
+        punkty[9] = 75;
+        console.log("Mały strit");
+    }
+     if(kostki[0] === 1 && kostki[1] === 1 && kostki[2] === 1 && kostki[3] === 1 && kostki[4] === 1){
+        punkty[10] = 95;
+        console.log("Duży  strit");
+    }
         if(kostki[i] >= 4){
             punkty[7] = ((i+1) * 15);
             console.log(`Czwórka ${i + 1}`);
@@ -161,12 +182,27 @@ function znajdzFigury(){
         if(kostki[i] === 5){
             punkty[11] = ((i+10)*10);
             console.log("Generał");
-        }
-        for( let j = 0; j < 6 ; j++){
-            
-        }
-    }
-   
+        } 
+    } 
     
+    
+}
+
+function przypisywanieWynikow(){
+    document.getElementById("jedynki").textContent = punkty[0];
+    document.getElementById("dwojki").textContent = punkty[1];
+    document.getElementById("trojki").textContent = punkty[2];
+    document.getElementById("czworki").textContent = punkty[3];
+    document.getElementById("piatki").textContent = punkty[4];
+    document.getElementById("szostki").textContent = punkty[5];
+    document.getElementById("trzyTeSame").textContent = punkty[6];
+    document.getElementById("czteryTeSame").textContent = punkty[7];
+    document.getElementById("full").textContent = punkty[8];
+    document.getElementById("malyStrit").textContent = punkty[9];
+    document.getElementById("duzyStrit").textContent = punkty[10];
+    document.getElementById("general").textContent = punkty[11];
+    document.getElementById("szansa").textContent = punkty[12];
+    document.getElementById("sumaPkt").textContent = punkty[13];
+ 
 }
 
