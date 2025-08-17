a1a5d9308585e090568006eec1c4a38eb8ce226f
Jajestemtymtypem <jajestemtymtypem@gmail.com>
2025-08-17 02:07:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let figura=[ 0, 0, 0, 0, 0];
 const obrazKostki = [null, "icons/1.png", "icons/2.png", "icons/3.png", "icons/4.png", "icons/5.png", "icons/6.png"]
 let czyMoznaRzucac = true;
 let wynik = 0;
+let punkty = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let tura = 0;
+let kostki = [0, 0, 0, 0, 0, 0];
 const stanKostki = true;
 function rzutKostkaLosowo(){ 
     return Math.floor(Math.random() * 6) + 1;
@@ -17,23 +19,27 @@ if(dice[i] !== "hold"){
     figura[i] = wynikKostki
     const img = document.getElementById([i]);
     img.src = obrazKostki[wynikKostki];
- 
+    kostki = [0, 0, 0, 0, 0, 0];
 }
     }
 tura++;
-if(tura>3){
-        tura = 1;
+zliczanieKosci();
+znajdzFigury();
+if(tura > 3){
+        tura = 0;
         dice=[ 0, 0, 0, 0, 0];
         figura=[ 0, 0, 0, 0, 0];
         document.querySelectorAll('.dice-box.selected')
         .forEach(b => b.classList.remove('selected'));
-       ;
-
+        kostki = [0, 0, 0, 0, 0, 0];
+        return;
     }
+      
 console.log(dice);
 console.log(tura);
 console.log(figura);
-
+console.log(`kostki ${kostki}`);
+console.log(`punkty ${punkty}`);
 }
 
 function hold0(el){
@@ -93,10 +99,74 @@ function hold4(el){
             console.log(dice);
         }else{
             dice[4] = figura[4];
-        }    
+        }
+         
 }}
-/*function znajdowanieFigur(){
+function zliczanieKosci(){
    for(let i = 0; i < 5; i++){
-    figura[i] = 
+    if(figura[i] === 1){
+        kostki[0] += 1; 
+    }    
+    if(figura[i] === 2){
+        kostki[1] += 1; 
+    }    
+    if(figura[i] === 3){
+        kostki[2] += 1; 
+    }    
+    if(figura[i] === 4){
+        kostki[3] += 1; 
+    }    
+    if(figura[i] === 5){
+        kostki[4] += 1; 
+    }    
+    if(figura[i] === 6){
+        kostki[5] += 1; 
+    }      
    }
-}*/
+}
+function znajdzFigury(){
+    if(kostki[0] !== 0){
+      punkty[0] = kostki[0] * 1;  
+    }
+    if(kostki[1] !== 0){
+      punkty[1] = kostki[1] * 2;  
+    }
+    if(kostki[2] !== 0){
+      punkty[2] = kostki[2] * 3;  
+    }
+    if(kostki[3] !== 0){
+      punkty[3] = kostki[3] * 4;  
+    }
+    if(kostki[4] !== 0){
+      punkty[4] = kostki[4] * 5;  
+    }
+    if(kostki[5] !== 0){
+      punkty[5] = kostki[5] * 6;  
+    }
+    for( let i = 0; i < 6; i++){
+        if(kostki[i] >= 3){
+            punkty[6] = ((i+1) * 10);
+            console.log(`Trójka ${i + 1}`);
+            for( let j = 0; j < 6; j++){
+                if(kostki[j] === 2&& kostki[j] != kostki[i]){
+                    punkty[8] = ((i + j) * 8);
+                    console.log(`Full ${i} i ${j}`);
+                }
+            }
+        }
+        if(kostki[i] >= 4){
+            punkty[7] = ((i+1) * 15);
+            console.log(`Czwórka ${i + 1}`);
+        }
+        if(kostki[i] === 5){
+            punkty[11] = ((i+10)*10);
+            console.log("Generał");
+        }
+        for( let j = 0; j < 6 ; j++){
+            
+        }
+    }
+   
+    
+}
+
